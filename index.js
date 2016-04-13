3a2a83b36ece8da03c5eeafcde2e07bd037887ca
Roman <grumpy@yandex-team.ru>
2016-04-14 03:02:35 +0500

    gzip, cache, cdn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('body-parser').json());
+app.use(require('compression')());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/layout/index.html');
@@ -29,8 +30,11 @@ app.post('/rum', (req, res) => {
     res.json({ result: 'ok' });
 });
 
-app.use(express.static('static'));
+const oneMonth = 1000 * 60 * 60 * 24 * 30;
+const cacheOptions = { maxAge: oneMonth };
 
-app.use('/vendor', express.static('node_modules'));
+app.use(express.static('static', cacheOptions));
+
+app.use('/vendor', express.static('node_modules', cacheOptions));
 
 app.listen(process.env.NODE_PORT || '8080');
